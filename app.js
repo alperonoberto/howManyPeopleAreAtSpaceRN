1617e4d6bb2bfe3e24df668974fc6cb872614303
alperonoberto <alperonoberto@gmail.com>
2022-06-24 21:34:49 +0200

    cambios generales en estética. Añadido link al repositorio de github

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const title = document.getElementById('title');
 const numContainer = document.getElementById('number-container');
 const num = document.getElementById('number');
-const astronautsContainer = document.getElementById('astronauts');
+const astronautsContainer = document.getElementById('astronautsContainer');
 const url = 'http://api.open-notify.org/astros.json';
+const gitLink = document.getElementById('github');
 
 const fetchAstronauts = async () => {
     const response = await fetch(url);
@@ -14,21 +15,29 @@ const renderAstronauts = data => {
     const astronauts = data.people;
     num.textContent = data.number;
 
-    console.log(astronauts);
+    // console.log(astronauts);
     
     for(let i = 0; i < astronauts.length; i++){
         const astDiv = document.createElement('div');
+        const astId = document.createElement('span')
         const astName = document.createElement('p');
         const astCraft = document.createElement('span');
+        const astPic = document.createElement('img');
 
         var astNum = 1;
 
         console.log(JSON.stringify(astronauts[i].name));
 
+        astId.textContent = i+1;
         astName.textContent = astronauts[i].name;
         astCraft.textContent = astronauts[i].craft;
+        astPic.setAttribute('src', `./images/${i+1}.jpg`);
+
+        astPic.style.width = '200px';
+        astPic.style.height = '200px';
 
         astDiv.classList.add(`astronaut-${astNum}`);
+        astDiv.appendChild(astId);
         astDiv.appendChild(astName);
         astDiv.appendChild(astCraft);
         astronautsContainer.appendChild(astDiv);
@@ -40,3 +49,7 @@ const renderAstronauts = data => {
 }
 
 fetchAstronauts();
+
+gitLink.addEventListener('click', () => {
+    window.open('https://github.com/alperonoberto/howManyPeopleAreAtSpaceRN');
+});
